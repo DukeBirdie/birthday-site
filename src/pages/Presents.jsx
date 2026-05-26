4ec48315f438e85c47312107518328d350aef339
Kyle Parker <kylerparker1500@gmail.com>
2026-05-26 16:40:21 -0700

    added confetti effect on User page load

--- src/pages/Presents.jsx
+++ src/pages/Presents.jsx
@@ -1,5 +1,15 @@
-function Presents() {
+import { Link } from 'react-router-dom'
 
+function Presents() {
+  return (
+    <>
+      <Link to={`/`} className='text-xl text-gray-300'>
+        <button className='inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20'>
+          View People 
+        </button>
+      </Link>
+    </>
+  );
 }
 
 export default Presents
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,58 @@
 import '../App.css'
+import { useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { TypeAnimation } from 'react-type-animation';
+import { confetti } from '@tsparticles/confetti'
 
 /**
  * Map to define different fonts for different users.
  */
 const users = {
-  "Sam": "font-sam",
-  "Sophie": "font-sophie",
-  "Bella": "font-bella",
+  "Sam": { 
+    "font": "font-sam",
+    "particleList": ["🍾", "🎁"],
+  },
+  "Sophie": {
+    "font": "font-sophie",
+    "particleList": ["🎉", "🍾"],
+  },
+  "Bella": {
+    "font": "font-bella",
+    "particleList": ["🍾", "🎊"]
+  },
 };
+
+// source: https://confetti.js.org/
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: .94,
+  startVelocity: 30
+};
+
+function shoot(name) {
+  confetti({
+    ...defaults,
+    particleCount: 15,
+    scalar: 1.2,
+    shapes: ["circle", "square"],
+    colors: [
+      "#a864fd",
+      "#29cdff",
+      "#78ff44",
+      "#ff718d",
+      "#fdff6a"
+    ]
+  });
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+    shapes: ["emoji"],
+    shapeOptions: { emoji: { value: users[name].particleList } }
+  });
+}
 
 
 /**
@@ -18,10 +61,17 @@ const users = {
  */
 function User() {
   const { name } = useParams();
-  const font = users[name] || 'font-sam';
+  const font = users[name].font || 'font-sam';
+
+  useEffect(() => {
+    setTimeout(shoot(name), 0);
+    setTimeout(shoot(name), 100);
+    setTimeout(shoot(name), 200);
+  }, [name])
 
   return (
     <>
+      {/* Do candles for age on this page -> blowing out animation */}
       <h1 className="font-light">
         Happy Birthday{' '}
         <span className={font}>
@@ -37,7 +87,7 @@ function User() {
 
       <Link to={`/presents/${name}`} className='text-xl text-gray-300'>
         <button className='inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20'>
-          View Presents
+          View Presents 🎁
         </button>
       </Link>
     </>
