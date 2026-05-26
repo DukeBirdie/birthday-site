40f2804f4450472e987f94a9ce51140301da7b08
Kyle Parker <kylerparker1500@gmail.com>
2026-05-26 03:29:28 -0700

    added simple typing animations

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,6 @@
 import '../App.css'
 import { useParams, Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation';
 
 /**
  * Map to define different fonts for different users.
@@ -21,8 +22,24 @@ function User() {
 
   return (
     <>
-      <h1 className="font-light">Happy Birthday <span className={font}>{(name === "Sam") ? "Samantha" : name}</span>!</h1>
-      <Link to={`/presents/${name}`}>View Presents</Link>
+      <h1 className="font-light">
+        Happy Birthday{' '}
+        <span className={font}>
+          <TypeAnimation
+            sequence={[(name === "Sam") ? "Samantha !🍾🎉" : name + " !🍾🎉", 1000]}
+            speed={10}
+            cursor={true}
+            repeat={0}
+            className="typing-cursor"
+          />
+        </span>
+      </h1>
+
+      <Link to={`/presents/${name}`} className='text-xl text-gray-300'>
+        <button className='inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20'>
+          View Presents
+        </button>
+      </Link>
     </>
   )
 }
