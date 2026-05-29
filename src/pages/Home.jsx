b59942a2f7c7d7247d55aaab15f0d632adb98428
Kyle Parker <kylerparker1500@gmail.com>
2026-05-29 01:10:43 -0700

    dark mode fix...?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import '../App.css'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
 function Home() {
   const names = ["Sam", "Sophie", "Bella"];
@@ -7,9 +9,18 @@ function Home() {
   return (
     <>
       <section id="center">
+        <h1>
+          <TypeAnimation
+            sequence={["Click on your name..."]}
+            speed={20}
+            cursor={true}
+            repeat={0}
+            className="typing-cursor font-medium text-left text-3xl transform-x-20"
+          />
+        </h1>
         <ul className="list-none text-left text-3xl text-white">
           {names.map(name => (
-            <li key={name} className='transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
+            <li key={name} className='transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110' >
               <Link to={`/user/${name}`}>
                 {name}
               </Link>
